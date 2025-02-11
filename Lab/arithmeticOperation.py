3b6aa642c68c2a49b3ad6e9b5afe5f4d53c12577
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-11 23:10:21 +0530

    performed basic arithmetic operations

---
+++ Lab/arithmeticOperation.py
@@ -0,0 +1,12 @@
+#1.Arithmetic operators
+
+num1 = 15
+num2 = 4
+
+print("Addition:", num1 + num2)         
+print("Subtraction:", num1 - num2)      
+print("Multiplication:", num1 * num2)   
+print("Division:", num1 / num2)         
+print("Exponentiation:", num1 ** num2)  
+print("Modulus:", num1 % num2)          
+print("Floor Division:", num1 // num2)
---
+++ Lab/comparison.py
@@ -0,0 +1,9 @@
+
+#3.compariosn operators
+# Example: Comparing Student Marks
+mark1 = 85
+mark2 = 92
+
+print("Is mark1 equal to mark2?", mark1 == mark2)  # False
+print("Is mark1 greater than mark2?", mark1 > mark2)  # False
+print("Is mark1 less than or equal to mark2?", mark1 <= mark2)  # True
