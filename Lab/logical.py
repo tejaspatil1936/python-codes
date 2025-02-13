2c125b35fd54984d8909f073217de63612e5b8f3
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-13 22:10:08 +0530

    organised codes

--- Lab/logical.py
+++
@@ -1,5 +0,0 @@
-# 4.Logical Operators
-a = 50
-b = 10
-
-print(a > b)  # True
--- Lab/membership.py
+++
@@ -1,5 +0,0 @@
-# 7.Membership Operators
-fruits = ["apple", "banana", "cherry"]
-
-print("Is 'apple' in the list?", "apple" in fruits)  # True
-print("Is 'grape' not in the list?", "grape" not in fruits)
