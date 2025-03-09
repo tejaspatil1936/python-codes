9bcdbe4eaac66f72e5ce395b8f3452d3e5684ef0
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-03-09 12:00:00 +0530

    codes

---
+++ opratorPrecedance.py
@@ -0,0 +1,14 @@
+# Given variable values
+A = 7
+B = 10
+C = 30
+D = 4
+E = 5
+# Evaluating expressions
+print("(i) (A + B) * C =", (A + B) * C) #90
+print("(ii) A + (B - E) =", A + (B - E)) #25
+print("(iii) A * B / E =", A * B / E) #40
+print("(iv) C // B // 5 =", C // B // 5) #0
+print("(v) C + (A * E) / (B - A) =", C + (A * E) / (B - A)) #20
+print("(vi) A ** E / 5 + C - D =", A ** E / 5 + C - D) #46
+print(~A)
