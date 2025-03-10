46fddb8c62791824bb33092d714a72b0a38f5dac
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-03-10 12:00:00 +0530

    codes

---
+++ pattern*.py
@@ -0,0 +1,67 @@
+# *
+# **
+# ***
+# ****
+# *****
+
+# num = int(input("Enter the number : "))
+# for i in range(1, num+1):  
+#     for j in range(i):  
+#         print('*', end='')  
+#     print()  
+
+###########################
+
+#    * 
+#   * * 
+#  * * * 
+# * * * * 
+
+# num = int(input("Enter the number : "))
+
+# for i in range(1, num):
+#     for j in range(1, num - i):
+#         print(" ", end='')
+#     for k in range(1, i+1):
+#         print('* ', end='')
+#     print()
+
+###########################
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+# num = int(input("Enter the number : "))
+
+# for i in range(1, num):
+#     for j in range(1, num - i):
+#         print(" ", end='')
+#     for k in range(1, i+1):
+#         print('*', end='')
+#     print()
+
+###########################
+
+# * * * * * 
+#  * * * * 
+#   * * * 
+#    * * 
+#     * 
+
+# num = int(input("Enter the number : "))
+
+# for i in range(num, 0, -1):
+#     for j in range(1, num-i+1):
+#         print(" ", end='')
+#     for k in range(1, i+1):
+#         print("* ", end='')
+#     print()
+
+# num = int(input("Enter the number: "))
+
+# for i in range(num, 0, -1):  
+#     print(" " * (num - i) + "* " * i) 
+
