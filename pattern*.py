10e302b11d8227fa6309b5f805dbe5029c280ac1
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-03-10 12:00:00 +0530

    codes

--- pattern*.py
+++ pattern*.py
@@ -4,11 +4,11 @@
 # ****
 # *****
 
-# num = int(input("Enter the number : "))
-# for i in range(1, num+1):
-#     for j in range(i):
-#         print('*', end='')
-#     print()
+num = int(input("Enter the number : "))
+for i in range(1, num+1):
+    for j in range(i):
+        print('*', end='')
+    print()
 
 ###########################
 
